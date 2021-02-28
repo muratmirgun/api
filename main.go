afd77ac0b529d05f8ba80ba9c38603e4b4f013dd
Murat Mirgün Ercan <muratmirgunercan225@gmail.com>
2021-02-28 21:26:45 +0300

    edits

--- main.go
+++ main.go
@@ -19,7 +19,7 @@ func main() {
 		return c.String(http.StatusOK, "")
 	})
 	e.GET("/users/:id", router.GetUser(db))
-	e.POST("/users/new/:id", router.PostUser(db))
+	//e.POST("/users/new/:id", router.PostUser(db))
 	e.DELETE("/users/delete/:id", deletes)
 	//logger and server start
 	e.Logger.Fatal(e.Start(":4000"))
