d7618a2bc59af7da059b4f6d6f93eee3d85237b1
Murat Mirgün Ercan <muratmirgunercan225@gmail.com>
2021-02-28 15:55:46 +0300

    main file edited

--- main.go
+++ main.go
@@ -1,25 +1,28 @@
 package main
 
 import (
+	"api/router"
 	"github.com/labstack/echo/v4"
 	"net/http"
 )
 
 func main() {
+	// router imports
+    gets := router.GetUser
+    posts := router.PostUser
+    deletes := router.DeleteUser
 
+    // Router
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "")
 	})
-	e.GET(
-		"/users/:id",
-		getUser,
-	)
+	e.GET("/users/:id", gets)
+    e.POST("/users/new/:id", posts)
+    e.DELETE("/users/delete/:id", deletes)
+	//logger and server start
 	e.Logger.Fatal(e.Start(":4000"))
 
 }
-func getUser(c echo.Context) error {
-	// User ID from path `users/:id`
-	id := c.Param("id")
-	return c.String(http.StatusOK, id)
-}
+
+
