8fef12a90f66cbb3c7c6dd6ffa9981944ff683d0
Murat Mirgün Ercan <muratmirgunercan225@gmail.com>
2021-02-28 18:49:34 +0300

    db get user func added but add user not working

--- router/router.go
+++ router/router.go
@@ -1,15 +1,21 @@
 package router
 
 import (
+	"api/models"
+	"database/sql"
 	"github.com/labstack/echo/v4"
 	"net/http"
-
+	"strconv"
 )
 
-func GetUser(c echo.Context) error {
-	id := c.Param("id")
-	sum := id + "get user"
-	return c.String(http.StatusOK, sum)
+type H map[string]interface{}
+
+func GetUser(db *sql.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		id, _ := strconv.Atoi(c.Param("id"))
+		// Fetch comments using our new model
+		return c.JSON(http.StatusOK, models.GetUsers(db, id))
+	}
 }
 
 func PutUser(c echo.Context) error {
@@ -17,11 +23,18 @@ func PutUser(c echo.Context) error {
 	sum := id + "Put user"
 	return c.String(http.StatusOK, sum)
 }
+/* bug TO do
+func PostUser(db *sql.DB) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		id, _ := strconv.Atoi(c.Param("id"))
+        db := models.InitDB("../sqlite.db")
+		models.AddUser(db, id, "new")
 
-func PostUser(c echo.Context) error {
-	id := c.Param("id")
-	return c.String(http.StatusOK,id )
+		return c.String(http.StatusOK,"OKEY")
+	}
 }
+*/
+
 
 func DeleteUser(c echo.Context) error {
 	id := c.Param("id")
